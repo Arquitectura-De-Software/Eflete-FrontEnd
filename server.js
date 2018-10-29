e04e99b40d0051a3075f3138a5eb14d681975124
Juanma <juanmanuelvallejos2@gmail.com>
2018-10-29 19:18:42 -0300

    Vuelvo a solucion anterior de server.js pero esta vez pegandole

--- server.js
+++ server.js
@@ -1,28 +1,4 @@
- var http = require('http');
- var fs = require('fs');
- var path = require('path');
-
- http.createServer(function (request, response) {
-
-    console.log('request starting for ');
-    console.log(request);
-
-    var filePath = '.' + request.url;
-    if (filePath == './')
-        filePath = './index.html';
-
-    console.log(filePath);
-    var extname = path.extname(filePath);
-    var contentType = 'text/html';
-    switch (extname) {
-        case '.js':
-            contentType = 'text/javascript';
-            break;
-        case '.css':
-            contentType = 'text/css';
-            break;
-    }
-
- }).listen(process.env.PORT || 5000);
-
- console.log('Server running.....');
+var express = require('express');
+var app = express();
+app.use(express.static(__dirname + '/'));
+app.listen(process.env.PORT || 5000);

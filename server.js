1ddc3c901bbdb69a1d38b735a8aca40bc2e01bf6
Juanma <juanmanuelvallejos2@gmail.com>
2018-10-29 18:51:55 -0300

    Se prueba otra variante de server.js

--- server.js
+++ server.js
@@ -1,10 +1,9 @@
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
+var express = require('express');
+var app = express();
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+const host = '0.0.0.0';
+const port = process.env.PORT || 3000;
+
+app.listen(port, host, function() {
+  console.log("Server started.......");
+});

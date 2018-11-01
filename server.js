3582b5fade9e962a86fb49f8357c1e1b39dc4d3f
Juanma <juanmanuelvallejos2@gmail.com>
2018-10-31 21:21:06 -0300

    Pruebo otro server.js

--- server.js
+++ server.js
@@ -1,4 +1,14 @@
-var express = require('express');
-var app = express();
-app.use(express.static(__dirname + '/'));
-app.listen(process.env.PORT || 5000);
+const express = require('express');
+const path = require('path');
+const port = process.env.PORT || 5000;
+const app = express();
+
+// the __dirname is the current directory from where the script is running
+app.use(express.static(__dirname));
+
+// send the user to index html page inspite of the url
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'index.html'));
+});
+
+app.listen(port);
